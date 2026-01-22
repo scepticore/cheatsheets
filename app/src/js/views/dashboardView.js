ca9fbf2314a22713761c927d2f1790e06542cf7d
urbenand <urbenandreas@gmail.com>
2026-01-22 15:18:26 +0100

    Test for editor

--- app/src/js/views/dashboardView.js
+++ app/src/js/views/dashboardView.js
@@ -1,9 +1,13 @@
 import {renderTemplate} from "../utils/templateEngine";
 
-export function showDashboard() {
+export async function showDashboard() {
   const example = {
     "title": "Dashboard",
     "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto at cumque eius modi molestias officiis possimus quaerat quam quibusdam temporibus! Assumenda autem ipsa officia porro quibusdam repudiandae rerum sint velit!",
   };
-  return renderTemplate("dashboard/index.html", {example});
+  await renderTemplate("dashboard/index.html", {example});
+
+  const editor = ace.edit("editor");
+  editor.setTheme("ace/theme/github_dark");
+  editor.session.setMode("ace/mode/markdown");
 }
