946b9fd7776427204c645dd5629175b77189f575
urbenand <urbenandreas@gmail.com>
2026-03-05 19:23:38 +0100

    Minor fix for .env loading in constants.js

--- app/src/js/constants.js
+++ app/src/js/constants.js
@@ -1,4 +1,2 @@
-import "dotenv/config";
-
-export const API_BASE = process.env.VITE_API_URL || "http://localhost:3030/api";
-export const HOST = process.env.HOST || "http://localhost:5173";
+export const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:3030/api";
+export const HOST = import.meta.env.HOST || "http://localhost:5173";
