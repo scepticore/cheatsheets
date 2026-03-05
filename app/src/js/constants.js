ea8d23b662a6b55a8a3f2fa302ddfe765219ed7f
urbenand <urbenandreas@gmail.com>
2026-03-05 19:31:54 +0100

    Debug for templateEngine.js

--- app/src/js/constants.js
+++ app/src/js/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:3030/api";
-export const HOST = import.meta.env.LOCALHOST || "http://localhost:5173";
+export const API_BASE = import.meta.env.VITE_API_URL;
+export const HOST = import.meta.env.LOCALHOST;
